8310f8aae65a207faa71293bea38cdaed49d61c1
obellish <obellish@mpilabels.com>
2024-12-27 14:59:16 -0500

    still no work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use cxx::UniquePtr;
+
 #[cxx::bridge(namespace = "org::blobstore")]
-mod ffi {}
+mod ffi {
+
+	struct BlobMetadata {
+		size: usize,
+		tags: Vec<String>,
+	}
+
+	extern "Rust" {
+		type MultiBuf;
+
+		fn next_chunk(buf: &mut MultiBuf) -> &[u8];
+	}
+
+	unsafe extern "C++" {
+		include!("include/blobstore.h");
+
+		type BlobstoreClient;
+
+		fn new_blobstore_client() -> UniquePtr<BlobstoreClient>;
+	}
+}
+
+pub struct MultiBuf {}
 
 fn main() {
 	println!("Hello, world!");
